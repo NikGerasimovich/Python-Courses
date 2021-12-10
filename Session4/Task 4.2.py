40de9583a82613e53f1d1480ff90ef78cda3a72b
Герасимович Никита Юрьевич <herasimovich_mu@peleng.jsc.local>
2021-12-10 11:17:11 +0300

    Рабочая программа по поискау полиндрома без реверсивных функций

--- Session4/Task 4.2.py
+++ Session4/Task 4.2.py
@@ -1,20 +1,51 @@
-### Task 4.2
-##Write a function that check whether a string is a palindrome or not. Usage of any reversing functions is prohibited. To check your implementation you can use
-##strings from [here](https://en.wikipedia.org/wiki/Palindrome#Famous_palindromes).
+# Task 4.2 Write a function that check whether a string is a palindrome or not. Usage of any reversing functions
+# is prohibited. To check your implementation you can use strings from [here](
+# https://en.wikipedia.org/wiki/Palindrome#Famous_palindromes).
 
 polindrom = str(input())
-polindromglobal = list(polindrom)
-def polindrom_calc(polindrom):
-#Фун-ия вычисления полиндрома.
-# Слово будет проверяться по середине. Если первая часть(допустим слово топот) == второй части(в перевернутом виде)тогда выведет True, иначе False
+
+
+def polindrom_doubl(polindrom):
+    # Фун-ия вычисления полиндрома. Слово будет проверяться по середине. Если первая часть(допустим слово топот) ==
+    # второй части(в перевернутом виде)тогда выведет True, иначе False
+
     tmppol = polindrom
     if len(tmppol) % 2 == 0:
-        global polindromglobal
-        halfpolindrom1 = polindrom[len(polindrom)//2 + 1:]
-        halfpolindrom2 = polindrom[len(polindrom)//2:]
+        halfpolindrom1 = polindrom[:(len(polindrom) // 2)]
+        halfpolindrom2 = polindrom[len(polindrom) // 2:]
+    else:
+        halfpolindrom1 = polindrom[:(len(polindrom) // 2) + 1]
+        halfpolindrom2 = polindrom[len(polindrom) // 2:]
+
+    halfpolindrom2 = list(halfpolindrom2)
+    halfpolindrom2 = halfpolindrom2[::-1]
+    halfpolindrom2 = ''.join(halfpolindrom2)
+    halfpolindrom3 = halfpolindrom2
+
+    if halfpolindrom1 == halfpolindrom3:
+        return print("Это полиндром:", polindrom)
+    else:
+        return print("Это не полиндром:", polindrom)
+
+polindrom_doubl(polindrom)
+"""
+def revers_polindrom(halfpolindrom2):
+    polindrom_doubl(polindrom)
+    global halfpolindrom3
+    halfpolindrom2 = list(halfpolindrom2)
+    halfpolindrom2 = halfpolindrom2[::-1]
+    halfpolindrom2 = ''.join(halfpolindrom2)
+    halfpolindrom3 = halfpolindrom2
+
+    return halfpolindrom3
 
 
+def calc_polindrom(halfpolndrom1, halfpolindrom3):
+    polindrom_doubl(polindrom)
 
-        return print(halfpolindrom1, halfpolindrom2)
+    if halfpolndrom1 == halfpolindrom3:
+        return print("Это полиндром:", polindrom)
+    else:
+        return print("Это не полиндром:", polindrom)
+"""
 
-polindrom_calc(polindrom)
