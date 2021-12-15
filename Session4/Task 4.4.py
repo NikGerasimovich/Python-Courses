ce0e29c101283b322293b6dd8224edbc1d5ed0da
Герасимович Никита Юрьевич <herasimovich_mu@peleng.jsc.local>
2021-12-15 10:46:18 +0300

    Функция разбиения строки по индексам

--- Session4/Task 4.4.py
+++ Session4/Task 4.4.py
@@ -11,35 +11,35 @@ import string
 
 inp_string = str(input())
 numb_of_index = [int(i) for i in input("Введите нужные индексы через пробел:").split()]
-dict_index = dict.fromkeys(numb_of_index)
 
 
-def split_index(inp_string, dict_index):
+def split_index(inp_string):
     # Функция разбивает строку по введённую пользователям по индексам которые определяет тоже пользователь
-
+    dict_index = dict.fromkeys(numb_of_index)
+    last_key = list(dict_index.keys())[-1]
     a = 0
     cut_string = tuple()
 
-
     for i in range(len(inp_string)):
-
         for j in dict_index:
             if i == j:
                 sep_string = ''.join(inp_string[a:j])
                 cut_string = cut_string + (sep_string,)
                 a = j
 
-    tmp = list(dict_index.keys())[-1]
-    last_key = tmp
-    cut_string = cut_string + (inp_string[last_key:],)
+            elif j > len(inp_string):
+                inp_string = "".join(inp_string)
+                cut_string = list()
+                cut_string.append(inp_string)
+                return cut_string
+
     cut_string = list(cut_string)
+    cut_string = cut_string + (inp_string[last_key:],)
 
     return cut_string
 
 
-print(split_index(inp_string, dict_index))
-
-# !!!!!!!!!!Доделать проверку на неправильные индексы(if index > len(inp_data) return inp_data )!!!!!!!!!!!!!!!!!!!
+print(split_index(inp_string))
 
 # Функция проходит по строке и словарю, где ключи словаря это индексы введенные пользователем.
 # Далее она делает срез от начала до превого индекса и значение запис в кортеж(потому что он не изменяемый),
