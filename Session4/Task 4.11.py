44ee7a16240a92448931e47025194ffd90f64b5a
Герасимович Никита Юрьевич <herasimovich_mu@peleng.jsc.local>
2021-12-29 16:04:14 +0300

    Программа принимает из InitTask11 словари и в функции обьед их в один где в случ одинак ключей складывает вместе их значение

--- Session4/Task 4.11.py
+++ Session4/Task 4.11.py
@@ -4,11 +4,24 @@ import InitTask11
 
 
 def sum_dict(*dict_argument):
+    # Функция обьеден словари в 1 и суммир их значения если ключи одинаковые
 
+    out_dict = dict()
     for dic in dict_argument:
-        dct = dict.fromkeys(dic)
+        for key in dic:
+            try:
+                out_dict[key] += dic[key]
+            except KeyError:
+                out_dict[key] = dic[key]
 
-    return print(dct)
+    return print(out_dict)
 
 
-sum_dict(InitTask11.dict1, InitTask11.dict2, InitTask11.dict3)
+sum_dict(InitTask11.dict1, InitTask11.dict2, InitTask11.dict3,
+         InitTask11.dict4, InitTask11.dict5, InitTask11.dict6,
+         InitTask11.dict7, InitTask11.dict8, InitTask11.dict9,
+         InitTask11.dict10, InitTask11.dict11, InitTask11.dict12,
+         InitTask11.dict13, InitTask11.dict14, InitTask11.dict15)
+
+# Данная программа принимает 15 словаоей из InitTask11 и затем последовательно подает их в функция,где обьеденяет их
+# в один словарь (в случае одинаковых ключей их значение суммируется)
